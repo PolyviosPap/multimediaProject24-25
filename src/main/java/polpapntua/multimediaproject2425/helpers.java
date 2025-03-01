a3758d5075b356ef9366694a006c297318765c92
PolyviosPap <ppap1996@gmail.com>
2025-03-01 19:18:39 +0200

    version 1.0.0 --javadoc helpers class

--- src/main/java/polpapntua/multimediaproject2425/helpers.java
+++ src/main/java/polpapntua/multimediaproject2425/helpers.java
@@ -14,9 +14,18 @@ import java.io.*;
 import java.util.List;
 import java.util.Objects;
 
+/** A utility class providing helper methods for general use.*/
 public class helpers {
+    /** Logger instance for recording events and errors. */
     protected static final Logger logger = LogManager.getLogger();
 
+    /**
+     * Creates a button of a specified icon.
+     *
+     * @param iconPath the path to the desired icon image
+     * @return a button with the specified icon
+     * @throws NullPointerException if the icon resource is not found
+     */
     public static Button createButton(String iconPath) {
         ImageView imageView = new ImageView(new Image(Objects.requireNonNull(helpers.class.getResourceAsStream(iconPath))));
         imageView.setFitWidth(20);
@@ -28,6 +37,13 @@ public class helpers {
         return button;
     }
 
+    /**
+     * Serializes an object to a JSON file.
+     *
+     * @param filePath the file path where the object will be saved
+     * @param object   the object to be serialized
+     * @param <T>      the type of the object (must be serializable)
+     */
     public static <T extends Serializable> void serializeObject(String filePath, T object) {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
@@ -40,6 +56,13 @@ public class helpers {
         }
     }
 
+    /**
+     * Serializes a list of objects to a JSON file.
+     *
+     * @param filePath the file path where the objects will be saved
+     * @param objects  the list of objects to be serialized
+     * @param <T>      the type of objects in the list (must be serializable)
+     */
     public static <T extends Serializable> void serializeObjects(String filePath, List<T> objects) {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT); // Pretty-print JSON
@@ -51,7 +74,17 @@ public class helpers {
         }
     }
 
+    /**
+     * A custom converter that safely converts strings to integers.
+     * If the input string is null, empty, or not a valid integer, it returns null.
+     */
     public static class SafeIntegerStringConverter extends IntegerStringConverter {
+        /**
+         * Converts a string to an integer.
+         *
+         * @param string the string to convert
+         * @return the integer value, or {@code null} if the string is empty or invalid
+         */
         @Override
         public Integer fromString(String string) {
             try {
@@ -62,6 +95,12 @@ public class helpers {
         }
     }
 
+    /**
+     * Displays an alert dialog with a warning message.
+     *
+     * @param title   the title of the alert dialog
+     * @param message the message to be displayed
+     */
     public static void showAlert(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.setTitle(title);
